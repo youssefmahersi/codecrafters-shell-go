fea42b1bdb1dc87f1b4cf6f76d90aff2b3d9748b
youssef_mahersi <youssefmahersi0@gmail.com>
2025-12-14 12:47:38 +0100

    feat : Implement echo

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,10 +13,17 @@ func main() {
 
 		fmt.Print("$ ")
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		args := strings.Split(command, " ")
 		if command[:len(command)-1] == "exit" {
 			stop = true
 			break
 		}
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		switch args[0] {
+		case "echo":
+			fmt.Printf("%s", args[1])
+		default:
+			fmt.Println(command[:len(command)-1] + ": command not found")
+		}
+
 	}
 }
