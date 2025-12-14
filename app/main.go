fbb675e8d93540a3e3cb5df3d19f9e29a935f6da
youssef_mahersi <youssefmahersi0@gmail.com>
2025-12-14 12:34:33 +0100

    feat : Implement a REPL

--- app/main.go
+++ app/main.go
@@ -6,12 +6,16 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
-
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
-	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	fmt.Println(command[:len(command)-1] + ": command not found")
+	var stop = false
+	for stop != true {
+
+		fmt.Print("$ ")
+		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		if command[:len(command)-1] == "exit" {
+			stop = true
+			break
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
+	}
 }
